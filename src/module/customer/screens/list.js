b1429fd22768b98bdfd7ca00001cba212e477843
thiago.simao <thiago.simao@iterative.com.br>
2022-04-15 21:29:04 -0300

    add page customer

---
+++ src/module/customer/screens/list.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const List = () => (
+   <h1> list view </h1>
+)
+
+export default List
---
+++ src/module/customer/screens/sign-up.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignUp = () => (
+   <h1> signUp </h1>
+)
+
+export default SignUp
