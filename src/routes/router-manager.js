44b9afbc7fb608b64979e35f9cf295d2b68f8ae2
thiago.simao <thiago.simao@iterative.com.br>
2022-04-15 21:29:31 -0300

    Create router

---
+++ src/routes/router-manager.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CustomerRouter } from '../module'
+import { Switch } from 'react-router'
+
+const Routes = () => (
+  <Switch>
+    <CustomerRouter />
+  </Switch>
+)
+
+export default Routes
